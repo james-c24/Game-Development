d8a7897588af7be55890c33ffe8da903ea1ba277
JamesC24 <jameschoung18@gmail.com>
2025-09-26 18:45:05 +0100

    Create Tuples.py

---
+++ GameDevelopment/Tuples.py
@@ -0,0 +1,19 @@
+my_tuple = ("apple","grape","orange","mango","kiwi",True,89.7)
+print(my_tuple)
+
+address = ("255","Buckwheat Avenue","New York","234567")
+
+for i in address:
+    print(i)
+
+house_number, street_name, city_name, pin_code = address
+print(house_number)
+print(street_name)
+print(city_name)
+print(pin_code)
+
+my_tuple = "apple", "banana", "mango", "kiwi"
+print(my_tuple)
+
+nested_tuple = ("apple","orange","banana",(1,2,3),(4,5,6),True)
+print(nested_tuple)
